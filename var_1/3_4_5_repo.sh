0734d8433a93e6f315505f2b30ba51103eefa139
projectrom <lrv495@gmail.com>
2023-01-09 12:23:57 -0800

    first variant

---
+++ var_1/3_4_5_repo.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+# Run this script with root privileges!
+
+DIR=`pwd`
+
+# Copy deb to repo
+/bin/mkdir -p apt-repo/pool/main
+/bin/cp peervpn_1.0_amd64.deb apt-repo/pool/main
+
+# Create Packages file
+/bin/mkdir -p apt-repo/dists/stable/main/binary-amd64
+cd apt-repo
+dpkg-scanpackages --arch amd64 pool/ > dists/stable/main/binary-amd64/Packages
+
+# Generate Release file
+cd dists/stable
+$DIR/generate-release.sh > Release
+
+# Configure repository
+echo "deb [arch=amd64] http://127.0.0.1:8000/apt-repo stable main" > /etc/apt/sources.list.d/example.list
+
+# Start web server
+cd $DIR
+python3 -m http.server &
+sudo apt-get update --allow-insecure-repositories
+
+# Install package
+apt install peervpn -y --allow-unauthenticated
